ddb4c48773ac57ffc9921b4a1acfddf04adb5345
paulrogers <paulr909@proton.me>
2023-04-01 21:20:51 +0100

    edit settings.py

--- project/settings.py
+++ project/settings.py
@@ -85,8 +85,8 @@ USE_TZ = True
 STATIC_URL = "static/"
 STATIC_ROOT = BASE_DIR / "staticfiles"
 STATICFILES_DIRS = [BASE_DIR / "static"]
-MEDIA_URL = "media/"
-MEDIA_ROOT = "media/"
+MEDIA_URL = "/media/"
+MEDIA_ROOT = BASE_DIR / "media"
 
 LOGIN_REDIRECT_URL = "home"
 LOGOUT_REDIRECT_URL = "home"
